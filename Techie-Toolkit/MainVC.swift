6f083c9580344624e1f6857bfbaa1660e3432413
sslack3130 <underwood.dj.steve@gmail.com>
2016-01-23 00:45:42 +0000

    Login Completed
    Login has been completed and authenticated against Firebase

---
+++ Techie-Toolkit/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  MainVC.swift
+//  Techie-Toolkit
+//
+//  Created by Steve Slack on 22/01/2016.
+//  Copyright © 2016 Steve Slack. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
